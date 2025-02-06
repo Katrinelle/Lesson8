808ba92005fc2fa9edd4f1cb542bc5aaa322e77e
Kateryna Navozenko <katrinelle19@gmail.com>
2025-02-06 02:20:47 +0200

    Add files via upload

--- Lesson/js/script.js
+++ Lesson/js/script.js
@@ -1,90 +1,182 @@
+// -----
 // let elem = document.getElementById("elem");
+// console.log(elem);
+// elem.style.background = "red";
+// elem.style.color = "yellow";
 
-// elem.style.background = "green";
+// -----
 
 // let elements = document.querySelectorAll("ul>li:last-child");
+// for (let elem of elements) {
+//   alert(elem.innerHTML);
+// }
 
+// -----
+// створення елементу
+
+// let div = document.createElement("div");
+// let text = document.createTextNode("Hola!");
+
+// -----
+// створення повідомлень
+
+// // 1. Створіть елемент <div>
+// let div = document.createElement("div");
+
+// // 2. Задайте йому клас "alert"
+// div.className = "alert";
+
+// // 3. Наповніть <div> змістом
+// div.innerHTML =
+//   "<strong>Всім привіт!</strong> Ви прочитали важливе повідомлення.";
+
+// // метод append - вставити десь на сторінці (body)
+// document.body.append(div);
+
+// -----
+
+// let elem = document.getElementById("elem");
+// elem.style.background = "green";
+
+// -----
+// querySelectorAll
+
+// let elements = document.querySelectorAll("ul>li:last-child");
 // for (element of elements) {
 //   console.log(element);
-// }
+// } // видає в консоль останню дитину
+
+// -----
+// querySelector
 
 // let element = document.querySelector("li");
 // console.log(element);
 
+// -----
+
 // let element = document.getElementsByTagName("div");
-// console.log(element);
+// console.log(element); // видає коллекцію всіх div
+
+// -----
 
 // let textHidden = document.getElementById("textHidden");
-// textHidden.hidden = true;
+// textHidden.hidden = true; // текст зи сторінки ховається
+// console.log(textHidden);
+
+// -----
 
 // let message = document.getElementById("message");
+// console.log(message.value); // отримуємо текст з html.
+// // якщо без value - отримаємо повністю формулу
 
-// console.log(message.value);
+// -----
 
 // let text = document.querySelector(".text-message");
-// console.log(text.textContent);
+// console.log(text.textContent); // отримуємо текст з html.
+// // // якщо без textContent - отримаємо повністю формулу
+
+// -----
 
 // let text = document.querySelector(".text-message");
-// // console.log(text.textContent);
+// console.log(text.textContent); //отримуємо текст з html.
+// // // якщо без textContent - отримаємо повністю формулу
+// text.textContent = "Hello, Olga"; // змінюємо текст прям на сайті
 
-// text.textContent = "Hello, Olga!";
+// -----
+// міняємо стилі
 
 // let btn = document.querySelector(".button");
 
 // btn.style.backgroundColor = "red";
 
+// -----
+
 // let text = document.querySelector("#p_text");
+// console.log(text.classList); // всі class які є у елемента в HTML по цьому id
+// console.log(text.classList.contains("red")); // true - перевірили чі є class="red"
+// // у елемента по id="p_text"
 
-// console.log(text.classList);
-
-// console.log(text.classList.contains("red"));
-
+// // видаляємо якийсь class
 // text.classList.remove("red");
+// console.log(text.classList.contains("red")); // false
 
-// console.log(text.classList);
-
-// console.log(text.classList.contains("red"));
-
+// // додаємо class
 // text.classList.add("new");
+// console.log(text.classList); // є новий class="new"
 
-// console.log(text.classList);
+// -----
 
 // let image = document.querySelector(".image");
 
-// console.log(image.hasAttribute("src"));
+// console.log(image.hasAttribute("src")); // true - перевіряємо чи
+// //має цей елемент атрібут src
 
-// console.log(image.getAttribute("src"));
+// console.log(image.getAttribute("src")); // посилання на картинку
+// // get - показує атрібут
 
-// const item = document.createElement("a");
+// -----
+// DOM вузли
+
+// створюємо вузол
+// const item = document.createElement("a"); // створили елемент <a>
+// щоб його побачити на сторінці - треба додати деталей
+
 // item.href = "#";
 // item.classList.add("btn");
 // item.textContent = "3";
 
 // const nav = document.querySelector(".nav");
-// nav.appendChild(item);
+// nav.appendChild(item); // зявилось 3
+
+// // додаємо заголовок
 
 // const heading = document.createElement("h1");
 // heading.textContent = "Hello";
 
 // const container = document.querySelector(".conteiner");
-// container.insertBefore(heading, nav);
+// container.insertBefore(heading, nav); // Hello - на сайті
 
-// nav.removeChild(item);
+// видаляємо елемент
 
-// item.remove();
+// // старий метод (працює у всіх барузерах):
+// nav.removeChild(item); // зникла 3
 
+// новий метод
+// item.remove(); // зникла 3
+
+// // клонуємо елемент, щоб використати в іншому місці
 // const text = document.querySelector(".text");
+
 // const parent = document.querySelector(".parent");
 
 // const clone = text.cloneNode(true);
 
-// parent.appendChild(clone);
+// parent.appendChild(clone); // текст продублювався
 
+// -----
+
+// додаємо елемент:
+
+// --- варіант 1
 // const container = document.querySelector(".conteiner");
 
 // const text = `<p>Sorry</p>`;
 
-// container.innerHTML += text;
+// // container.innerHTML = text; // замінює заголовок Hello на Sorry
+// container.innerHTML += text; // додає заголовок Sorry після Hello
+
+// ---
+
+// --- варіант 2 (кращий, бо не перерендує усю сторінку)
+
+// const list = document.querySelector("#list");
+
+// list.insertAdjacentHTML("beforebegin", "<p>beforebegin</p>"); // beforebegin перед списком
+// list.insertAdjacentHTML("afterbegin", "<li>after</li>"); // after на початку списку
+// list.insertAdjacentHTML("beforeend", "<li>end</li>"); // end в кінці списку
+// list.insertAdjacentHTML("afterend", "<p>afterend</p>"); // afterend після списку
+
+// -----
 
 // const list = document.querySelector("#list");
 
